e87c39638b7154683e30ee017a1f50fc35534cb7
lovrodukic <81589029+lovrodukic@users.noreply.github.com>
2024-02-12 08:40:59 -0800

    Update request.js

--- expressjs-backend/routes/request.js
+++ expressjs-backend/routes/request.js
@@ -60,8 +60,8 @@ router.post("/account/registration", async (req, res) => {
     if (!matchingUser.length) {
       const user = await addUser(userToAdd);
       const safeUser = {
-        userid: esapi.encoder().encodeForHtml(user.userid),
-        password: esapi.encoder().encodeForHtml(user.password),
+        userid: esapi.encoder().encodeForHTML(user.userid),
+        password: esapi.encoder().encodeForHTML(user.password),
       };
       res.status(201).send(safeUser);
     } else {
